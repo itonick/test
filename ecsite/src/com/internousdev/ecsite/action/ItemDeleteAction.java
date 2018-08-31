ea30a5657d511405e2fe36e14750d5e25928cf95
itonick <iszemg@gmail.com>
2018-08-31 10:02:48 +0900

    8/31

--- ecsite/src/com/internousdev/ecsite/action/ItemDeleteAction.java
+++ ecsite/src/com/internousdev/ecsite/action/ItemDeleteAction.java
@@ -1,65 +1,11 @@
 package com.internousdev.ecsite.action;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Map;
-
-import org.apache.struts2.interceptor.SessionAware;
-
-import com.internousdev.ecsite.dao.ItemDeleteDAO;
-import com.internousdev.ecsite.dto.ItemDeleteDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class ItemDeleteAction extends ActionSupport implements SessionAware{
-	public Map<String,Object> session;
-	private ItemDeleteDAO itemDeleteDAO=new ItemDeleteDAO();
-	private ArrayList<ItemDeleteDTO> itemDeleteList=new ArrayList<ItemDeleteDTO>();
-	private String deleteFlg;
-	private String message;
-	public void delete() throws SQLException{
-		String item_name=session.get("itemName").toString();
-		String item_price=session.get("itemPrice").toString();
-		String item_stock=session.get("itemStock").toString();
-		int res=itemDeleteDAO.itemDelete(item_name,item_price,item_stock);
-
-		if(res>0){
-			itemDeleteList=null;
-			setMessage("商品情報を正しく削除しました。");
-		}else if(res==0){
-			setMessage("商品情報の削除に失敗しました。");
-		}
+public class ItemDeleteAction extends ActionSupport{
+	public String execute(){
+		String result=SUCCESS;
+		return result;
 	}
-	public Map<String, Object> getSession() {
-		return session;
-	}
-	public void setSession(Map<String, Object> session) {
-		this.session = session;
-	}
-
-	public ItemDeleteDAO getItemDeleteDAO() {
-		return itemDeleteDAO;
-	}
-	public void setItemDeleteDAO(ItemDeleteDAO itemDeleteDAO) {
-		this.itemDeleteDAO = itemDeleteDAO;
-	}
-	public ArrayList<ItemDeleteDTO> getItemDeleteList() {
-		return itemDeleteList;
-	}
-	public void setItemDeleteList(ArrayList<ItemDeleteDTO> itemDeleteList) {
-		this.itemDeleteList = itemDeleteList;
-	}
-	public String getDeleteFlg() {
-		return deleteFlg;
-	}
-	public void setDeleteFlg(String deleteFlg) {
-		this.deleteFlg = deleteFlg;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
 
 }
--- ecsite/src/com/internousdev/ecsite/action/ItemDeleteConfirmAction.java
+++ ecsite/src/com/internousdev/ecsite/action/ItemDeleteConfirmAction.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import org.apache.struts2.interceptor.SessionAware;
 
 import com.internousdev.ecsite.dao.ItemDeleteConfirmDAO;
-import com.internousdev.ecsite.dto.MyPageDTO;
+import com.internousdev.ecsite.dto.ItemDeleteConfirmDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class ItemDeleteConfirmAction extends ActionSupport implements SessionAware{
@@ -23,7 +23,7 @@ public class ItemDeleteConfirmAction extends ActionSupport implements SessionAwa
 		String item_price=session.get("item_price").toString();
 		String item_stock=session.get("item_stock").toString();
 
-		int res=ItemDeleteConfirmDAO.itemDelete(id,item_name,item_price,item_stock);
+		int res=itemDeleteConfirmDAO.ItemDelete(id,item_name,item_price,item_stock);
 
 		if(res>0){
 			itemDeleteConfirmDAO=null;
@@ -31,6 +31,34 @@ public class ItemDeleteConfirmAction extends ActionSupport implements SessionAwa
 		}else if(res==0){
 			setMessage("商品情報の削除に失敗しました。");
 		}
+	}
+
+	public Map<String, Object> getSession() {
+		return session;
+	}
+
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+
+	public ItemDeleteConfirmDAO getItemDeleteConfirmDAO() {
+		return itemDeleteConfirmDAO;
+	}
+
+	public void setItemDeleteConfirmDAO(ItemDeleteConfirmDAO itemDeleteConfirmDAO) {
+		this.itemDeleteConfirmDAO = itemDeleteConfirmDAO;
+	}
+
+	public ArrayList<ItemDeleteConfirmDTO> getItemDeleteConfirm() {
+		return itemDeleteConfirm;
+	}
+
+	public void setItemDeleteConfirm(ArrayList<ItemDeleteConfirmDTO> itemDeleteConfirm) {
+		this.itemDeleteConfirm = itemDeleteConfirm;
+	}
+
+	public String getDeleteFlg() {
+		return deleteFlg;
 	}
 
 	public void setDeleteFlg(String deleteFlg){
